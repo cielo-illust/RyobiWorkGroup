1c250f897d74a90ecb6aa30fbc89e4f0e2805686
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2017-06-04 22:45:10 +0900

    commit

---
+++ Assets/Scripts/WebView.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebView : MonoBehaviour {
+
+    private string url = "http://www.tamarun.jp/";
+    WebViewObject webViewObject;
+
+    // Use this for initialization
+    void Start () {
+        webViewObject = (new GameObject("WebViewObject")).AddComponent<WebViewObject>();
+        webViewObject.Init((msg) => {
+            Debug.Log(msg);
+        });
+        webViewObject.LoadURL(url);
+        webViewObject.SetMargins(0, 0, 0, 0);
+        webViewObject.SetVisibility(true);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
