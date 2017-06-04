72821b770c26d5ee1e0c5fd2f8093a04d9b83bd3
cielo-illust <the_illustrations_of_cielo@hotmail.co.jp>
2017-06-04 16:20:22 +0900

    first commit

---
+++ Assets/RyobiWorkGroupController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RyobiWorkGroupController : MonoBehaviour {
+
+    public Text text;
+    public int clickFlag = 0;
+
+
+    // Use this for initialization
+    void Start () {
+        DateTime thisDay = DateTime.Now;
+        text.text = thisDay.ToString("G");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (clickFlag == 0)
+        {
+            DateTime thisDay = DateTime.Now;
+            text.text = thisDay.ToString("G");
+        }
+    }
+
+    public void ButtonOnClick ()
+    {
+        if (clickFlag == 0)
+        {
+            clickFlag = 1;
+        } else
+        {
+            clickFlag = 0;
+        }
+    }
+}
